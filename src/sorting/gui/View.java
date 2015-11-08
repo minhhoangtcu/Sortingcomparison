603b41d91b19cbb6453a795b2265fd34887e3f5e
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-11-08 15:56:27 -0600

    Added a file chooser button and get/set methods for view.

--- src/sorting/gui/View.java
+++ src/sorting/gui/View.java
@@ -1,8 +1,5 @@
 package sorting.gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -13,29 +10,18 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JList;
-import java.awt.GridLayout;
 import java.awt.Font;
 
 public class View extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textFieldSize;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					View frame = new View();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JButton btnChooseFile;
+	private JButton btnSort;
+	private JList<String> list;
+	private JLabel lblFeedBack;
+	private JLabel lblFile;
+	private JTextPane paneHTMLOutput;
 
 	/**
 	 * Create the frame.
@@ -51,7 +37,7 @@ public class View extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, "cell 0 0 3 1,grow");
 		
-		JTextPane paneHTMLOutput = new JTextPane();
+		paneHTMLOutput = new JTextPane();
 		scrollPane.setViewportView(paneHTMLOutput);
 		
 		JPanel panelInput = new JPanel();
@@ -65,26 +51,50 @@ public class View extends JFrame {
 		panelInput.add(textFieldSize, "cell 1 0,growx");
 		textFieldSize.setColumns(10);
 		
-		JButton btnSort = new JButton("Sort");
-		panelInput.add(btnSort, "cell 2 0 1 2,grow");
+		btnSort = new JButton("Sort");
+		panelInput.add(btnSort, "cell 2 0,grow");
 		
 		JLabel lblSortingAlgorithm = new JLabel("Sorting Algorithm");
 		panelInput.add(lblSortingAlgorithm, "cell 0 1");
 		
-		JList list = new JList();
+		list = new JList<String>();
 		panelInput.add(list, "cell 1 1,grow");
+		
+		btnChooseFile = new JButton("Choose File");
+		panelInput.add(btnChooseFile, "cell 2 1");
 		
 		JPanel panelFeedback = new JPanel();
 		contentPane.add(panelFeedback, "cell 2 1,grow");
 		panelFeedback.setLayout(new MigLayout("", "[100%]", "[50%][50%]"));
 		
-		JLabel lblFeedBack = new JLabel("Feed Back");
+		lblFeedBack = new JLabel("Feed Back");
 		lblFeedBack.setFont(new Font("Tahoma", Font.BOLD, 12));
 		panelFeedback.add(lblFeedBack, "cell 0 0,alignx center,growy");
 		
-		JLabel lblFile = new JLabel("file");
+		lblFile = new JLabel("file");
 		lblFile.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		panelFeedback.add(lblFile, "cell 0 1,alignx right,growy");
 	}
 
+	public JButton getBtnChooseFile() {
+		return btnChooseFile;
+	}
+	public JButton getBtnSort() {
+		return btnSort;
+	}
+	public int getTextFieldSize() throws NumberFormatException {
+		return Integer.parseInt(textFieldSize.getText());
+	}
+	public String getListSelect() {
+		return list.getSelectedValue();
+	}
+	public void setLblFeedBack(String text) {
+		lblFeedBack.setText(text);
+	}
+	public void setLblFile(String text) {
+		lblFile.setText(text);;
+	}
+	public JTextPane getPaneHTMLOutput() {
+		return paneHTMLOutput;
+	}
 }
