93a223b5e04084167ed47337cbf38077037cf0bb
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-11-09 00:45:04 -0600

    Added documents

--- src/reader/AlphanumericReader.java
+++ src/reader/AlphanumericReader.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 public class AlphanumericReader {
 
 	/**
-	 * 
-	 * @param size
-	 * @param path
-	 * @return
-	 * @throws IOException 
+	 * <p>Take a file path and the number of lines to read. Then, the program will return
+	 * an array containing all the keys(lines). 
+	 * @param size number of keys/lines the method is going to read from the file
+	 * @param path path to the file
+	 * @return an array containing all the keys 
+	 * @throws IOException error indicating that the method cannot find the file with the provided path
 	 */
 	public static String[] getKeys(int size, String path) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(path));
