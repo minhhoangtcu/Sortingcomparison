c53b03e475f19e169de53123818788f91228064c
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-11-09 01:13:38 -0600

    Changed the GUI. Button colors. Lables visiability.

--- src/sorting/gui/View.java
+++ src/sorting/gui/View.java
@@ -11,6 +11,7 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JList;
 import java.awt.Font;
+import java.awt.Color;
 
 public class View extends JFrame {
 
@@ -56,6 +57,7 @@ public class View extends JFrame {
 		textFieldSize.setColumns(10);
 		
 		btnSort = new JButton("Sort");
+		btnSort.setBackground(Color.WHITE);
 		panelInput.add(btnSort, "cell 2 0,grow");
 		
 		JLabel lblSortingAlgorithm = new JLabel("Sorting Algorithm");
@@ -65,17 +67,21 @@ public class View extends JFrame {
 		panelInput.add(list, "cell 1 1,grow");
 		
 		btnChooseFile = new JButton("Choose File");
+		btnChooseFile.setBackground(Color.WHITE);
 		panelInput.add(btnChooseFile, "cell 2 1");
 		
 		JPanel panelFeedback = new JPanel();
 		contentPane.add(panelFeedback, "cell 2 1,grow");
 		panelFeedback.setLayout(new MigLayout("", "[100%]", "[50%][50%]"));
 		
-		lblFeedBack = new JLabel("Feed Back");
+		lblFeedBack = new JLabel("Feedback");
+		lblFeedBack.setVisible(false);
+		lblFeedBack.setForeground(Color.RED);
 		lblFeedBack.setFont(new Font("Tahoma", Font.BOLD, 12));
 		panelFeedback.add(lblFeedBack, "cell 0 0,alignx center,growy");
 		
 		lblFile = new JLabel("file");
+		lblFile.setVisible(false);
 		lblFile.setFont(new Font("Tahoma", Font.ITALIC, 11));
 		panelFeedback.add(lblFile, "cell 0 1,alignx right,growy");
 	}
@@ -89,13 +95,29 @@ public class View extends JFrame {
 	public int getTextFieldSize() throws NumberFormatException {
 		return Integer.parseInt(textFieldSize.getText());
 	}
+	public boolean isSizeValid() {
+		String text = textFieldSize.getText();
+		if (text.isEmpty()) {
+			return false;
+		} else {
+			try {
+				Integer.parseInt(text);
+				return true;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}
+	}
 	public String getListSelect() {
 		return list.getSelectedValue();
 	}
 	public void setLblFeedBack(String text) {
+		lblFeedBack.setVisible(true);
 		lblFeedBack.setText(text);
 	}
+	
 	public void setLblFile(String text) {
+		lblFile.setVisible(true);
 		lblFile.setText(text);;
 	}
 	public JTextPane getPaneHTMLOutput() {
