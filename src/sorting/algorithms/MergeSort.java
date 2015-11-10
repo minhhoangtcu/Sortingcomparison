35ebd84f231b229198403a7b058aeef71aab1af4
minhhoangtcu <minh.hoang@tcu.edu>
2015-11-10 11:54:15 -0600

    Initiated Merge Sort with description.

---
+++ src/sorting/algorithms/MergeSort.java
@@ -0,0 +1,19 @@
+package sorting.algorithms;
+
+public class MergeSort<T extends Comparable<T>> extends SortingAlgorithm<T> {
+	
+	public MergeSort() {
+		super();
+	}
+	
+	@Override
+	protected void sort(T[] input) {
+		
+	}
+
+	@Override
+	public String getDescription() {
+		return "Mergesort is a divide and conquer algorithm.";
+	}
+
+}
