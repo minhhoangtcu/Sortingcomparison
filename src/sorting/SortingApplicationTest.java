f037e9335120a8cf8fdf5162a7307ba74721a874
minhhoangtcu <minh.hoang@tcu.edu>
2015-11-13 17:01:03 -0600

    Chnaged work station

--- src/sorting/SortingApplicationTest.java
+++ src/sorting/SortingApplicationTest.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 
 import reader.AlphanumericReader;
 import reader.ReaderTest;
+import sorting.algorithms.HeapSort;
 import sorting.algorithms.MergeSort;
+import sorting.algorithms.QuickSort;
+import sorting.algorithms.SelectionSort;
 import sorting.algorithms.SortingAlgorithm;
+import sorting.algorithms.SortingMethod;
 
 public class SortingApplicationTest {
 	
@@ -17,24 +21,10 @@ public class SortingApplicationTest {
 		String pathRandom = ReaderTest.RANDOM;
 		
 		// 5000; Merge; Random
-		int size = 5000;
-		app.model.setKeys(AlphanumericReader.getKeys(size, pathRandom));
-		String[] keys = app.model.getKeys();
-		MergeSort<String> mS = new MergeSort<>(keys);
-		System.out.println("Merge Sort");
-		System.out.println("Sizes: " + size);
-		System.out.print("\tCompares: " + mS.getCompares());
-		System.out.print("\tMoves: " + mS.getMoves());
+		SortingAlgorithm<String> sA = getSorting(SortingMethod.MERGE, pathRandom, 5000);
+		printSorting(sA);
 		
 		// 2000; Merge; Random
-		size = 2000;
-		app.model.setKeys(AlphanumericReader.getKeys(size, pathRandom));
-		keys = app.model.getKeys();
-		mS = new MergeSort<>(keys);
-		System.out.println("Merge Sort");
-		System.out.println("Sizes: " + size);
-		System.out.print("\tCompares: " + mS.getCompares());
-		System.out.print("\tMoves: " + mS.getMoves());
 		
 		// 1000; Merge; Random
 		size = 1000;
@@ -59,11 +49,29 @@ public class SortingApplicationTest {
 		app.view.setLblFile(pathRandom);
 	}
 
+	public static SortingAlgorithm<String> getSorting(SortingMethod m, String path, int size) throws IOException {
+		String[] keys = AlphanumericReader.getKeys(size, path);
+		
+		switch (m) {
+		case HEAP:
+			return new HeapSort<String>(keys);
+		case INSERTION:
+		case MERGE:
+			return new MergeSort<String>(keys);
+		case QUICK:
+			return new QuickSort<String>(keys);
+		case SELECTION:
+			return new SelectionSort<String>(keys);
+		case SHELL:
+		default:
+			return null;
+		}
+	}
 	
 	
-	public static void printSorting(SortingAlgorithm s, int size, String path) throws IOException {
+	public static void printSorting(SortingAlgorithm<?> s) {
 		System.out.println(s.getSortingMethod().getName());
-		System.out.println("Sizes: " + size);
+		System.out.println("Sizes: " + s.getLength());
 		System.out.print("\tCompares: " + s.getCompares());
 		System.out.print("\tMoves: " + s.getMoves());
 	}
