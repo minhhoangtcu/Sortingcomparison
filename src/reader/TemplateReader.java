f3e47878043cd719a2f51afb3438a50182e2e90c
Minh Hoang TCU <minh.hoang@tcu.edu>
2015-11-14 22:02:41 -0600

    Print out all results at the start of the program

---
+++ src/reader/TemplateReader.java
@@ -0,0 +1,44 @@
+package reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TemplateReader {
+	
+	public enum Template {
+		CONTENT_LIST_TEMPLATE,
+		SORTING_NAME_TEMPLATE,
+		ONE_SORTING_ALGORITHMS;
+	}
+	
+	public static String read(Template type) throws IOException {
+		FileReader htmlTemplateFile;
+		switch (type) {
+		case CONTENT_LIST_TEMPLATE:
+			htmlTemplateFile = new FileReader("templates/contentListTemplate.html");
+			break;
+		case ONE_SORTING_ALGORITHMS:
+			htmlTemplateFile = new FileReader("templates/OneAlgorithm.html");
+			break;
+		case SORTING_NAME_TEMPLATE:
+			htmlTemplateFile = new FileReader("templates/sortingNameTemplate.html");
+			break;
+		default:
+			htmlTemplateFile = null;
+		}
+		return getContent(htmlTemplateFile);
+	}
+	
+	private static String getContent(FileReader file) throws IOException {
+		BufferedReader reader = new BufferedReader(file);
+		StringBuilder builder = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			builder.append(line + "\n");
+		}
+		return builder.toString();
+	}
+	
+
+}
--- src/sorting/SortingApplicationTest.java
+++ src/sorting/SortingApplicationTest.java
@@ -1,10 +1,11 @@
 package sorting;
 
 import java.io.IOException;
-
 import reader.AlphanumericReader;
 import reader.ReaderTest;
-import sorting.algorithms.HeapSortBook;
+import reader.TemplateReader;
+import reader.TemplateReader.Template;
+import sorting.algorithms.HeapSort;
 import sorting.algorithms.MergeSort;
 import sorting.algorithms.QuickSort;
 import sorting.algorithms.SelectionSort;
@@ -20,26 +21,47 @@ public class SortingApplicationTest {
 		
 		int[] sizes = {100, 1000, 2000, 5000};
 		String[] paths = {ReaderTest.RANDOM, ReaderTest.ASCENDING, ReaderTest.DESCENDING};
-		SortingMethod[] methods = {SortingMethod.INSERTION, SortingMethod.MERGE, SortingMethod.QUICK};
+		SortingMethod[] methods = { SortingMethod.SELECTION, SortingMethod.MERGE, SortingMethod.QUICK,
+				SortingMethod.HEAP };
 		
+//		for (SortingMethod method: methods) {
+//			for (String file: paths) {
+//				System.out.println(method.getName() + "| " + file);
+//				for (int size: sizes) {
+//					printSorting(getSorting(method, file, size));
+//				}
+//				System.out.println();
+//			}
+//		}
+		
+		StringBuilder builder = new StringBuilder();
 		for (SortingMethod method: methods) {
 			for (String file: paths) {
-				System.out.println(method.getName() + "| " + file);
+				builder.append(getSortingNameHTML(method, file));
 				for (int size: sizes) {
-					printSorting(getSorting(method, file, size));
+					builder.append(getSortingContentHTML(getSorting(method, file, size)));
 				}
-				System.out.println();
 			}
 		}
+		app.view.setPaneHTMLOutput(builder.toString());
 	}
 
-	public static SortingAlgorithm<String> getSorting(SortingMethod m, String path, int size) throws IOException {
+	private static String getSortingNameHTML(SortingMethod method, String file) throws IOException {
+		String html = TemplateReader.read(Template.SORTING_NAME_TEMPLATE);
+		String name = method.getName();
+		html = html.replace("$name", name);
+		html = html.replace("$file", file);
+		return html;
+	}
+
+	private static SortingAlgorithm<String> getSorting(SortingMethod m, String path, int size) throws IOException {
 		String[] keys = AlphanumericReader.getKeys(size, path);
 		
 		switch (m) {
 		case HEAP:
-			return new HeapSortBook<String>(keys);
+			return new HeapSort<String>(keys);
 		case INSERTION:
+			return null;
 		case MERGE:
 			return new MergeSort<String>(keys);
 		case QUICK:
@@ -47,16 +69,26 @@ public class SortingApplicationTest {
 		case SELECTION:
 			return new SelectionSort<String>(keys);
 		case SHELL:
+			return null;
 		default:
 			return null;
 		}
 	}
 	
-	
-	public static void printSorting(SortingAlgorithm<?> s) {
-		System.out.println("Sizes: " + s.getLength());
-		System.out.print("\tCompares: " + s.getCompares());
-		System.out.print("\tMoves: " + s.getMoves() + "\n");
-	}
+//	private static void printSorting(SortingAlgorithm<?> s) {
+//		System.out.println("Sizes: " + s.getLength());
+//		System.out.print("\tCompares: " + s.getCompares());
+//		System.out.print("\tMoves: " + s.getMoves() + "\n");
+//	}
 
+	private static String getSortingContentHTML(SortingAlgorithm<?> s) throws IOException {
+		String html = TemplateReader.read(Template.CONTENT_LIST_TEMPLATE);
+		int sizes = s.getLength();
+		int compares = s.getCompares();
+		int moves = s.getMoves();
+		html = html.replace("$keys", ""+sizes);
+		html = html.replace("$compares", ""+compares);
+		html = html.replace("$moves", ""+moves);
+		return html;
+	}
 }
